db92869d99a40a9397561bc39b581593264b4da0
aalloul <adam@adamdataservices.com>
2025-01-03 11:30:21 +0100

    fix bug dans prix_instantane_carburant.py

--- src/prix_instantane_carburant.py
+++ src/prix_instantane_carburant.py
@@ -2,14 +2,15 @@ import requests
 
 
 def telecharger_donnes_economie_gouv(dataset):
-    url = f"https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/{dataset}/records?select=id%2Clatitude%2Clongitude%2Ccp%2Cadresse%2Cville%2Cservices%2Cgazole_prix%2Cgazole_maj%2Choraires%2Csp95_maj%2Csp95_prix%2Csp98_maj%2Csp98_prix&limit={limit}&offset={offset}"
-    toutes_les_data = []
     step = 100
     offset = 0
 
+    url = f"https://data.economie.gouv.fr/api/explore/v2.1/catalog/datasets/{dataset}/records?select=id%2Clatitude%2Clongitude%2Ccp%2Cadresse%2Cville%2Cservices%2Cgazole_prix%2Cgazole_maj%2Choraires%2Csp95_maj%2Csp95_prix%2Csp98_maj%2Csp98_prix&limit={{step}}&offset={{offset}}"
+    toutes_les_data = []
+
     print("Télécharger les données")
     while True:
-        r = requests.get(url.format(limit=step, offset=offset))
+        r = requests.get(url.format(step=step, offset=offset))
         r.raise_for_status()
         data = r.json()
         toutes_les_data += data['results']
