520028527a11aa54efbc6f1ec87a813886f9ce77
aalloul <adam@adamdataservices.com>
2025-02-06 15:23:48 +0100

    Premier tests unitiaires

---
+++ tests/test_lecteur_configuration.py
@@ -0,0 +1,13 @@
+import pytest
+from utils.lecteur_configuration import retrouver_sql
+
+
+def test_retrouver_sql_fichier_existant():
+    res = retrouver_sql("test_sql")
+    res_attendu = "SELECT"
+    assert res == res_attendu
+
+
+def test_retrouver_sql_fichier_inexistant():
+    with pytest.raises(FileNotFoundError):
+        retrouver_sql("nom_fichier")
