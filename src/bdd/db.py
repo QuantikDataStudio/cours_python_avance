337332bcdb9211e0dfff7cbb75cc6982ac2e0a4e
aalloul <adam@adamdataservices.com>
2025-02-14 15:54:24 +0100

    correction erreur

--- src/bdd/db.py
+++ src/bdd/db.py
@@ -1,9 +1,9 @@
 import duckdb
 
 
-def stocker_dans_bdd(sql, fichier, bdd):
+def stocker_dans_bdd(sql, fichier, bdd, nom_table):
     print("Chargement dans la BDD")
     with duckdb.connect(bdd) as connection:
         connection.sql(sql)
-        connection.sql('INSERT INTO consommation_brute_quotidienne_gaz_elec_raw '
+        connection.sql(f'INSERT INTO {nom_table} '
                        f'SELECT * FROM read_json_auto("{fichier}")')
