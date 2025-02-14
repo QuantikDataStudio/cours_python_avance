7dc61fbcf17c9eb092218e13bfaf3a7eebbecf00
aalloul <adam@adamdataservices.com>
2025-02-14 15:52:22 +0100

    correction erreur

--- src/main.py
+++ src/main.py
@@ -10,4 +10,5 @@ configuration = lire_configuration()
 for config in configuration:
     resultat = config.telecharger()
     stocker_fichier(resultat, config.fichier_cible)
-    stocker_dans_bdd(config.sql_creation, config.fichier_cible, fichier_base_de_donnees)
+    nom_table = config.fichier_sql.split("/")[-1].split(".")[0]
+    stocker_dans_bdd(config.sql_creation, config.fichier_cible, fichier_base_de_donnees, nom_table)
