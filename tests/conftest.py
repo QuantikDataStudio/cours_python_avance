b586e948753bae2a90c3f0d45766b114180a250c
aalloul <adam@adamdataservices.com>
2025-03-17 15:41:40 +0100

    fix test unitaire

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,6 +33,7 @@ def input_economie_gouv_fixture_select_vide():
             "fichier_cible": "fichier_cible",
             "fichier_sql": "fixtures/sql/test_sql",
             "sql_creation": "SELECT",
+            "nom_table": "nom_table",
             "select": []}
 
 
@@ -42,6 +43,7 @@ def input_economie_gouv_fixture_sans_select():
             "dataset": "dataset",
             "fichier_cible": "fichier_cible",
             "fichier_sql": "fixtures/sql/test_sql",
+            "nom_table": "nom_table",
             "sql_creation": "SELECT"}
 
 
@@ -52,6 +54,7 @@ def input_economie_gouv_fixture_avec_select():
             "fichier_cible": "fichier_cible",
             "fichier_sql": "fixtures/sql/test_sql",
             "sql_creation": "SELECT",
+            "nom_table": "nom_table",
             "select": ["id1", "id2"]}
 
 
@@ -61,7 +64,8 @@ def input_data_gouv_fixture():
             "dataset": "dataset",
             "fichier_cible": "fichier_cible",
             "fichier_sql": "fixtures/sql/test_sql",
-            "sql_creation": "SELECT"
+            "sql_creation": "SELECT",
+            "nom_table": "nom_table"
             }
 
 
